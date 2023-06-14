69823088722490c26c9273869f65a5e2ff92808c
Kirisamebiggestfan <136210082+Kirisamebiggestfan@users.noreply.github.com>
2023-06-13 21:42:41 -0500

    Added RegexCheckerRun and defined it.

--- Code/ClassLibrary/RegexChecker.cs
+++ Code/ClassLibrary/RegexChecker.cs
@@ -1,7 +1,34 @@
 ﻿namespace RegexPractice;
-public class Class1
+using System.Text.RegularExpressions;
+
+/// <summary>
+/// Prompts the user to enter a regular expression or press ENTER to use the default regular expression.
+/// Then the program prompts the user to enter an input to compare it to the regex.
+/// The programs compares the input and regular expression and returns either true or false.
+/// </summary>
+public class RegexChecker
 {
-    public static void Print() {
-        System.Console.WriteLine("Success");
+    public static bool RegexCheckerRun() {
+        string? regex, inputToMatch;
+        string defaultRegex = "^[a-z]";
+
+        // Query
+        System.Console.Write("The default regular expression checks for at least one digit.\nEnter a regular expression (or press ENTER to use the default): ");
+        regex = System.Console.ReadLine();
+
+        if (regex is null) 
+            throw new Exception("Null value");
+
+        System.Console.Write("Enter the input to match: ");
+        inputToMatch = System.Console.ReadLine();
+
+        if (inputToMatch is null)
+            throw new Exception("inputToMatch is null");
+
+        // If the regex is empty, then use default regex.
+        if (regex == "")
+            regex = defaultRegex;
+        
+        return Regex.IsMatch(inputToMatch, regex);
     }
 }
--- Code/Program/Program.cs
+++ Code/Program/Program.cs
@@ -2,6 +2,6 @@
 
 class Program {
     public static void Main() {
-        Class1.Print();
+        RegexChecker.RegexCheckerRun();
     }
 }
